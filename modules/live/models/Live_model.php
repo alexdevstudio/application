929a38d53b9e2c65f28c9aafeae2e291791a35e0
guru4all <gmaratos@gmail.com>
2017-04-13 09:38:48 +0300

    Added Ups Category on insert

--- modules/live/models/Live_model.php
+++ modules/live/models/Live_model.php
@@ -2218,7 +2218,7 @@ class Live_model extends CI_Model {
 				 $c == "routers"  || $c == "switches"  || $c == "laptops"  || $c== "desktops" || $c == "tablets"  || $c == "smartphones" ||
 				 $c == "cables" || $c == "patch_panels" || $c == "racks" || $c =="optical_drives" || $c == "card_readers" || $c == "flash_drives" || 
 				 $c == "power_supplies" || $c == "cases" || $c == "fans" || $c == "motherboards" || $c == "graphic_cards" || $c == "cpu" || 
-				 $c == "memories" || $c == "hoverboards" || $c =="printer_fusers" || $c =="printer_drums" || $c =="printer_belts"){
+				 $c == "memories" || $c == "hoverboards" || $c =="printer_fusers" || $c =="printer_drums" || $c =="printer_belts" || $c=="ups"){
 
 
 					$shipping_class = Modules::run('categories/makeShippingClass', $chars_array, $c);
