50eddd9ab6ad9e126676f208c79b30a111ab6659
guru4all <george@ittech.gr>
2018-02-20 14:21:28 +0200

    Added papers category

--- modules/categories/controllers/Categories.php
+++ modules/categories/controllers/Categories.php
@@ -47,7 +47,7 @@ class Categories extends MX_Controller {
     	$array = array('accessories','cables','cable_accessories','card_readers','carrying_cases','cartridges','cases','cooling_pads','copiers','cpu',
     		'desktops','docking_stations','external_hard_drives','fans','flash_drives','graphic_cards','hoverboards','ip_phones',
             'ip_cards','ip_gateways','ip_pbx', 'keyboard_mouse','laptops','memories','monitors','motherboards','multifunction_printers',
-    		'optical_drives','patch_panels','powerlines','power_bank','power_supplies','printers','printer_drums','printer_fusers','printer_belts','projectors','racks',
+    		'optical_drives','papers','patch_panels','powerlines','power_bank','power_supplies','printers','printer_drums','printer_fusers','printer_belts','projectors','racks',
 			'routers','sata_hard_drives','servers','server_controllers','server_cpu','server_hard_drives','server_memories','server_power_supplies',
 			'smartphones','software','speakers','ssd','switches','tablets','toners', 'tv', 'ups');
 
