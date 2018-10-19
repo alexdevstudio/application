cc33780d054b6caf4c6906ce206ab2dfa4b1e714
Alex <info@alexdev.gr>
2018-10-19 16:12:05 +0300

    Started filtered crosssells functionality, created view and form validation, deletion

--- modules/crosssales/controllers/Crosssales.php
+++ modules/crosssales/controllers/Crosssales.php
@@ -3,31 +3,69 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Crosssales extends MX_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	
+
+
+	 public function index()
+ 	{
+ 		if($this->input->post()){
+ 				$this->cross_validation();
+ 		}
+
+ 		$this->load->model('crosssales_model');
+
+		$data['tables'] = Modules::run('categories/fullCategoriesArray');
+		$data['title'] = 'Παράλληλα προϊόντα';
+ 		$data['filters'] = Modules::run('crud/get', 'cross_sells_similar');
+ 		$this->load->view('templates/header', $data);
+ 		$this->load->view('index');
+ 		$this->load->view('templates/footer');
+ 	}
 
 	public function auto_laptop($sku, $brand, $size, $price){
 
     	$this->load->model('crosssales_model');
-    	
+
     	$result = $this->crosssales_model->auto_laptop($sku, $brand, $size, $price);
     	return $result;
-    }
+  }
 
+	private function cross_validation(){
+
+		$this->form_validation->set_rules('category', 'Κατηηγορία', 'required');
+		$this->form_validation->set_rules('filter', 'Τμήμα τίτλου', 'required');
+		$this->form_validation->set_rules('skus', 'SKU προϊόντων', 'required');
+
+		if ($this->form_validation->run())
+			{
+					$_POST['skus'] = str_replace(" ", "", $_POST['skus']);
+					$this->db->insert('cross_sells_similar', $_POST);
+					$FlashData['Message']= '<strong>Το φίλτρο προστέθηκε.</strong>';
+					$FlashData['type'] = 'success';
+			}
+
+			if( ! isset($FlashData))	{
+					$FlashData['Message']= validation_errors('<p class=""> - ', '</p>');
+					$FlashData['type'] = 'danger';
+				}
+
+				$this->session->set_flashdata('flash_message', $FlashData);
+				return;
+	}
+
+	function delete($id){
+		$this->db->where('id', $id);
+		$this->db->delete('cross_sells_similar');
+		if($this->db->affected_rows() > 0){
+			$FlashData['Message']= '<strong>Το φίλτρο προστέθηκε.</strong>';
+			$FlashData['type'] = 'success';
+		}else{
+			$FlashData['Message']= '<strong>Το φίλτρο δεν βρέθηκε.</strong>';
+			$FlashData['type'] = 'danger';
+		}
+
+		$this->session->set_flashdata('flash_message', $FlashData);
+		redirect('crosssales');
+	}
 
 
 
@@ -39,4 +77,4 @@ class Crosssales extends MX_Controller {
 
 
 
-?> 
+?>
---
+++ modules/crosssales/views/index.php
@@ -0,0 +1,99 @@
+<br>
+  <div class="col-xs-12">
+    <?php
+       			 //flash messages
+    			    if($this->session->flashdata('flash_message')){
+
+    			      echo '<div class="alert alert-'. $this->session->flashdata('flash_message')['type'].'">';
+    			      echo '<a class="close" data-dismiss="alert">&times</a>';
+    			      echo $this->session->flashdata('flash_message')['Message'];
+    			      echo '</div>';
+    			    }
+    			?>
+  </div>
+
+<div class="col-md-4">
+          <div class="box box-success">
+            <div class="box-header with-border">
+              <h3 class="box-title">Προσθήκη νέου φίλτρου για παράλληλα προϊόντα</h3>
+            </div>
+            <!-- /.box-header -->
+            <form class="" action="crosssales" method="post">
+                <div class="box-body">
+                  <div class="form-group">
+                    <label for="category" class="">Επιλογή κατηγορίας</label>
+                    <select id="category" class="form-control" name="category">
+                      <option value=''>Κατηγορίες</option>
+                      <?php   foreach($tables as $table)  :  ?>
+                        <option value='<?= $table; ?>'><?= ucfirst($table); ?></option>
+                      <?php endforeach;  ?>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="filter" >Τμήμα τίτλου</label>
+                    <input type="text" name="filter" class="form-control" id="filter" value="<?= set_value('filter') ?>" placeholder="Τμήμα τίτλου...( DELL Inspiron 5)..." >
+                  </div>
+                  <div class="form-group">
+                    <label for="skus" >SKU των προϊόντων (χωρισμένα με κόμμα)</label>
+                    <input type="text" name="skus" class="form-control" id="skus" value="<?= set_value('skus') ?>" placeholder="13254848, 1315487, 1328751" >
+                  </div>
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+                <button type="submit" class="btn pull-right btn-success">Προσθήκη</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
+        </div>
+
+        <div class="col-md-8">
+                  <div class="box box-solid">
+                    <div class="box-header with-border">
+                      <h3 class="box-title">Ενεργά φίλτρα για παράλληλες πωλήσεις</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="row">
+                          <div class="col-xs-12">
+                            <table class="table table-striped" id="table">
+                            <thead>
+                            <tr>
+                              <th>Κατηγορία</th>
+                              <th>Τμήμα τίτλου</th>
+                              <th>SKUs</th>
+                              <th><i class="fa fa-times "></i></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                              <?php foreach ($filters->result() as $filter): ?>
+                                <tr>
+                                  <td><?= ucfirst( $filter->category ) ?></td>
+                                  <td><?= $filter->filter ?></td>
+                                  <td><?= $filter->skus ?></td>
+                                  <td><a href="crosssales/delete/<?= $filter->id ?>"><i class="fa fa-times text-red"></i></a></td>
+                                </tr>
+                              <?php endforeach; ?>
+
+                            </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
+                    <!-- /.box-body -->
+                  </div>
+                  <!-- /.box -->
+                </div>
+                <script>
+                  $(function () {
+                    $('#table').DataTable({
+                      'paging'      : true,
+                      'lengthChange': true,
+                      'searching'   : true,
+                      'ordering'    : true,
+                      'info'        : true,
+                      'autoWidth'   : false,
+                      "pageLength": 25
+                    })
+                  })
+                </script>
